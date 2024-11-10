e59152d5a0a4380b57a6b5009802aa46ae19f0f8
rafasantosbzr <rafasantosbzr@icloud.com>
2024-11-10 12:02:49 -0300

    adding a drawer

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header";
+import TemporaryDrawer from "./components/TemporaryDrawer";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TemporaryDrawer />
       <ListarTarefa />
     </div>
   );
